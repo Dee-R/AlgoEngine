cf8b21e2f1e56d9d692f966f7e4640e99159a417
Eddy Rogier <eddyrogier@gmail.com>
2020-08-27 00:15:49 +0200

    FizzBuzz

--- AlgoEngine/AlgoEngine/FizzBuzz.swift
+++ AlgoEngine/AlgoEngine/FizzBuzz.swift
@@ -5,6 +5,14 @@
 //  Created by Eddy R on 26/08/2020.
 //  Copyright © 2020 Eddy R. All rights reserved.
 //
+/*
+ 1
+ 2
+ 3 , 6, 9, 12 ...= Fizz
+ 4
+ 5 , 10 = Buzz
+ 15, = FizzBuzz
+ **/
 
 import Foundation
 import OSLog
@@ -14,9 +22,12 @@ protocol Game {
   func printMessageWith(_ value: String)
 }
 
+
+
 class GameEngine {
   let game: Game
   let valueLimitedTo: Int
+  var messageToPrint: String = ""
   
   init(valueLimitedTo: Int,gameMock: Game ) {
     self.game = gameMock
@@ -24,8 +35,19 @@ class GameEngine {
   }
   
   func start() {
-    if valueLimitedTo != 0 {
-      game.printMessageWith("1")
+    for i in 0...valueLimitedTo {
+      if i == 0 {
+      } else if i % 15 == 0 {
+        messageToPrint.append("FizzBuzz")
+      } else if i % 3 == 0 {
+        messageToPrint.append("Fizz")
+      } else if i % 5 == 0 {
+        messageToPrint.append("Buzz")
+      }
+      else {
+        messageToPrint.append(String(i))
+      }
     }
+    game.printMessageWith(messageToPrint)
   }
 }
--- AlgoEngine/FizzBuzzTest/FizzBuzzTest.swift
+++ AlgoEngine/FizzBuzzTest/FizzBuzzTest.swift
@@ -12,45 +12,81 @@ import XCTest
 
 class FizzBuzzTest: XCTestCase {
   
-//  func test_LimitTo_0() {
-//    let gameMock = GameMock()
-//    let sut = GameEngine(valueLimitedTo: 0, game: gameMock)
-//
-//    sut.start()
-//    XCTAssertEqual(gameMock.message, "")
-//  }
-//
-//
-//
-//  // -----
-//  class GameMock: Game {
-//    var message: String = ""
-//    func limitedValue(to: Int) {
-//
-//    }
-//  }
-  
+  let gameMock01 = GameMock()
   
   func test_startPrint_noValue() {
-    let gameMock01 = GameMock()
     let sut = GameEngine(valueLimitedTo: 0, gameMock: gameMock01)
     sut.start()
     XCTAssertEqual(gameMock01.message, "")
   }
   
   func test_startPrint_1() {
-    let gameMock01 = GameMock()
     let sut = GameEngine(valueLimitedTo: 1, gameMock: gameMock01)
     sut.start()
     XCTAssertEqual(gameMock01.message, "1")
   }
-  
+  func test_startPrint_2() {
+    let sut = GameEngine(valueLimitedTo: 2, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12")
+  }
+  func test_startPrint_3() {
+    let sut = GameEngine(valueLimitedTo: 3, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12Fizz")
+  }
+  func test_startPrint_4() {
+    let sut = GameEngine(valueLimitedTo: 4, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12Fizz4")
+  }
+  func test_startPrint_5() {
+    let sut = GameEngine(valueLimitedTo: 5, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12Fizz4Buzz")
+  }
+  func test_startPrint_6() {
+    let sut = GameEngine(valueLimitedTo: 6, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12Fizz4BuzzFizz")
+  }
+  func test_startPrint_7() {
+    let sut = GameEngine(valueLimitedTo: 7, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12Fizz4BuzzFizz7")
+  }
+  func test_startPrint_8() {
+    let sut = GameEngine(valueLimitedTo: 8, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12Fizz4BuzzFizz78")
+  }
+  func test_startPrint_9() {
+    let sut = GameEngine(valueLimitedTo: 9, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12Fizz4BuzzFizz78Fizz")
+  }
+  func test_startPrint_10() {
+    let sut = GameEngine(valueLimitedTo: 10, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12Fizz4BuzzFizz78FizzBuzz")
+  }
+  func test_startPrint_12() {
+    let sut = GameEngine(valueLimitedTo: 12, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12Fizz4BuzzFizz78FizzBuzz11Fizz")
+  }
+  func test_startPrint_15() {
+    let sut = GameEngine(valueLimitedTo: 15, gameMock: gameMock01)
+    sut.start()
+    XCTAssertEqual(gameMock01.message, "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz")
+  }
   
   class GameMock: Game {
     var message: String = ""
     
     func printMessageWith(_ value: String) {
       self.message = value
+      print(message)
     }
   }
 
